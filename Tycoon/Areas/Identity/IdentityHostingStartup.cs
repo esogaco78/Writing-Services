115df95a3a012c5076ecc0cf1e78aa02fd0df982
Madan <madangurrala@gmail.com>
2019-12-19 23:56:47 -0500

    ProjectCreated

---
+++ Tycoon/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tycoon.Data;
+
+[assembly: HostingStartup(typeof(Tycoon.Areas.Identity.IdentityHostingStartup))]
+namespace Tycoon.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
